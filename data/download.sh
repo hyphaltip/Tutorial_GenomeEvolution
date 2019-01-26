7dae2308f2c330e7deae7322e197c05df8710199
Jason Stajich <jasonstajich.phd@gmail.com>
2019-01-26 09:25:47 -0800

    download and organize, more compact download setup

--- data/download.sh
+++ data/download.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/bash
 
-mkdir proteindb
 RELEASE="41"
 URL="http://fungidb.org/common/downloads/release-${RELEASE}"
 # get comparative proteins
+PROTEINDIR=proteindb
+mkdir -p $PROTEINDIR
 while read SPECIES
 do
 	FILE="FungiDB-${RELEASE}_${SPECIES}_AnnotatedProteins.fasta"
-	OUTFILE=proteindb/${SPECIES}.aa.fasta
+	OUTFILE=$PROTEINDIR/${SPECIES}.aa.fasta
 	if [ ! -f $OUTFILE ]; then
 		curl -o $OUTFILE $URL/${SPECIES}/fasta/data/$FILE
 	fi
 done < protein_species.dat
 
-mkdir dnadb
+DNADIR=dnadb
+mkdir -p $DNADIR
+
 while read SPECIES
 do
 	DNAFILE="FungiDB-${RELEASE}_${SPECIES}_Genome.fasta"
-	OUTDNA=dnadb/${SPECIES}.dna.fasta
+	OUTDNA=$DNADIR/${SPECIES}.dna.fasta
 	GFFFILE="FungiDB-${RELEASE}_${SPECIES}.gff"
-	OUTGFF=dnadb/${SPECIES}.gff
+	OUTGFF=$DNADIR/${SPECIES}.gff
 
 	if [ ! -f $OUTDNA ]; then
 		 curl -o $OUTDNA $URL/${SPECIES}/fasta/data/$DNAFILE
@@ -28,5 +31,3 @@ do
 		curl -o $OUTGFF $URL/${SPECIES}/gff/data/$GFFFILE
 	fi
 done < dna_species.dat
-
-popd
