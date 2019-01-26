5d1a5943417d258c48b075093769275d2c8f88b2
Jason Stajich <jasonstajich.phd@gmail.com>
2019-01-26 09:08:11 -0800

    download steps

--- data/download.sh
+++ data/download.sh
@@ -1,34 +1,32 @@
 #!/usr/bin/bash
 
-if [ ! -f FungiDB-41_AfumigatusAf293.gff ]; then
-curl -O http://fungidb.org/common/downloads/Current_Release/AfumigatusAf293/gff/data/FungiDB-41_AfumigatusAf293.gff
-fi
-if [ ! -f FungiDB-41_AfumigatusA1163.gff ]; then
-curl -O http://fungidb.org/common/downloads/Current_Release/AfumigatusA1163/gff/data/FungiDB-41_AfumigatusA1163.gff
-fi
-if [ ! -f FungiDB-41_AnovofumigatusIBT16806.gff ]; then
-curl -O http://fungidb.org/common/downloads/Current_Release/AnovofumigatusIBT16806/gff/data/FungiDB-41_AnovofumigatusIBT16806.gff
-fi
+mkdir proteindb
+RELEASE="41"
+URL="http://fungidb.org/common/downloads/release-${RELEASE}"
+# get comparative proteins
+while read SPECIES
+do
+	FILE="FungiDB-${RELEASE}_${SPECIES}_AnnotatedProteins.fasta"
+	OUTFILE=proteindb/${SPECIES}.aa.fasta
+	if [ ! -f $OUTFILE ]; then
+		curl -o $OUTFILE $URL/${SPECIES}/fasta/data/$FILE
+	fi
+done < protein_species.dat
 
-if [ ! -f FungiDB-41_AfumigatusAf293_AnnotatedProteins.fasta ]; then
-	curl -O http://fungidb.org/common/downloads/Current_Release/AfumigatusAf293/fasta/data/FungiDB-41_AfumigatusAf293_AnnotatedProteins.fasta
-fi
-if [ ! -f FungiDB-41_AfumigatusA1163_AnnotatedProteins.fasta ]; then
-curl -O http://fungidb.org/common/downloads/Current_Release/AfumigatusA1163/fasta/data/FungiDB-41_AfumigatusA1163_AnnotatedProteins.fasta
-fi
+mkdir dnadb
+while read SPECIES
+do
+	DNAFILE="FungiDB-${RELEASE}_${SPECIES}_Genome.fasta"
+	OUTDNA=dnadb/${SPECIES}.dna.fasta
+	GFFFILE="FungiDB-${RELEASE}_${SPECIES}.gff"
+	OUTGFF=dnadb/${SPECIES}.gff
 
-if [ ! -f  FungiDB-41_AnovofumigatusIBT16806_AnnotatedProteins.fasta ]; then
-	curl -O http://fungidb.org/common/downloads/Current_Release/AnovofumigatusIBT16806/fasta/data/FungiDB-41_AnovofumigatusIBT16806_AnnotatedProteins.fasta
-fi
+	if [ ! -f $OUTDNA ]; then
+		 curl -o $OUTDNA $URL/${SPECIES}/fasta/data/$DNAFILE
+	fi
+	if [ ! -f $OUTGFF ]; then
+		curl -o $OUTGFF $URL/${SPECIES}/gff/data/$GFFFILE
+	fi
+done < dna_species.dat
 
-if [ ! -f FungiDB-41_AfumigatusA1163_Genome.fasta ]; then
-curl -O http://fungidb.org/common/downloads/Current_Release/AfumigatusA1163/fasta/data/FungiDB-41_AfumigatusA1163_Genome.fasta
-fi
-
-if [ ! -f FungiDB-41_AfumigatusAf293_Genome.fasta ]; then
-curl -O http://fungidb.org/common/downloads/Current_Release/AfumigatusAf293/fasta/data/FungiDB-41_AfumigatusAf293_Genome.fasta
-fi
-
-if [ ! -f FungiDB-41_AnovofumigatusIBT16806_Genome.fasta ]; then
-curl -O http://fungidb.org/common/downloads/Current_Release/AnovofumigatusIBT16806/fasta/data/FungiDB-41_AnovofumigatusIBT16806_Genome.fasta
-fi
+popd
