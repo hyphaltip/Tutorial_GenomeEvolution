033b91425f59e72ced280edce05987efa31fb6db
Jason Stajich <jason.stajich@gmail.com>
2019-01-30 12:11:25 +0100

    out fix cmds

--- scripts/install_pkg.R
+++ scripts/install_pkg.R
@@ -1,7 +1,6 @@
 install.packages("ggplot2","gridExtra","dplyr","RColorBrewer","pheatmap")
-
- if (!requireNamespace("BiocManager", quietly = TRUE)) 
-  install.packages("BiocManager") i
-BiocManager::install("AnnotationDbi", version = "3.8") 
-BiocManager::install("tximport", version = "3.8") 
+ if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("AnnotationDbi", version = "3.8")
+BiocManager::install("tximport", version = "3.8")
 
